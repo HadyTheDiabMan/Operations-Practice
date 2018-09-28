fc3c540ef34906d5311718b051b9b7401f56c0f4
hdiab22 <42975537+HadyTheDiabMan@users.noreply.github.com>
2018-09-28 12:27:34 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,29 @@
+
+public class OperationsTest
+{
+
+	public static void main(String[] args) 
+	{
+		int number; //Declaration statement
+		number = 20; //assignment statement -> Initialization
+		number=number+5; //number+=5  ----  mean the same thing
+		System.out.println(number);
+		number=number/2;
+		System.out.println(number);
+		number-=6;
+		System.out.println(number);
+		number*=4;
+		System.out.println(number);
+		number%=2;
+		System.out.println(number);
+		
+		
+		
+		
+		
+		
+				
+
+	}
+
+}
